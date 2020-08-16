e9b6023a01c7c54ad4cef885d8ea1a95e1e02316
younan <okokoknow@naver.com>
2020-08-17 00:36:59 +0900

    Longest Substring Without Repeating Characters - solution 1 : brute force.

--- AddTwoNumbers/Solution.java
+++ AddTwoNumbers/Solution.java
@@ -12,7 +12,7 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode p = l1, q = l2, curr = dummyHead;
-        int carry = 0; 
+        int carry = 0;
         while (p != null || q != null) {
             int x = (p != null) ? p.val : 0;
             int y = (q != null) ? q.val : 0;
